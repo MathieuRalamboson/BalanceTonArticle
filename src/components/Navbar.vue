22a61db4124ee0f5142290a5275cdd9ba9c72c96
MathieuRalamboson <ralamboson@et.esiea.fr>
2019-10-06 13:30:18 +0200

    NavBar Fonctionnel et Boutton sign out déplacer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <!-- Barre navigation -->
+  <nav>
+    <v-app-bar flat app>
+      <!-- NavBAR -->
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Mathieu.R</span>
+        <span>Vue.js</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-btn text color="grey" v-on:click="logout">
+        <span>Sign Out</span>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- Content a ajouter si besoin -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid></v-container>
+    </v-content>
+    <!-- Content a ajouter si besoin -->
+    <v-navigation-drawer app v-model="drawer" class="indigo">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+  <!-- Barre navigation -->
+</template>
+
+<script>
+import ToDo from "@/components/ToDo";
+import Router from "vue-router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+export default {
+  components: { ToDo },
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "mdi-account", text: "Login", route: "/" },
+        { icon: "mdi-folder", text: "View", route: "/page-1" },
+        { icon: "mdi-view-dashboard", text: "Autre", route: "/page-2" }
+      ]
+    };
+  },
+  methods: {
+    async logout() {
+      // Deconnection
+      console.log("Tentative de déconnection.");
+      const response = await this.axios.get("http://localhost:4000" + "/api/logout");
+      if (response) {
+        //Si il y a déconnection , go Login
+        this.$router.push("/");
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/Recup.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-form v-model="valid">
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field v-model="identifiant" label="Identifiant" required v-show="isActive"></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field v-model="mdp" label="Mdp" required v-show="isActive"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-btn v-on:click="checkLogin">Login</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-layout>
+     <img src="./liverpool.png" required v-show = "!isActive">
+  </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
